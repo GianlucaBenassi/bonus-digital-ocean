4fe1304b9c314a7aa0733f9977bcfdc6c472b446
Gianluca Benassi <benox1199@gmail.com>
2022-01-07 16:13:33 +0100

    CustomSlider layout

--- src/components/commons/CustomSlider.vue
+++ src/components/commons/CustomSlider.vue
@@ -1,11 +1,17 @@
 <template>
-    <Carousel>
-        <Slide>
-            contenuto
+    <Carousel 
+    :per-page="1"
+    :navigationEnabled="navigation"
+    :navigationPrevLabel='`<i class="fas fa-chevron-left fs-1"></i>`'
+    :navigationNextLabel='`<i class="fas fa-chevron-right fs-1"></i>`'
+    >
+        <Slide v-for="(info, index) in infos" :key="index">
+
+            <img class="w-100" :src="info.image" :alt="'slide image ' + index">
+            <p class="text-center mt-3">{{info.desc}}</p>
+
         </Slide>
-        <Slide>
-            contenuto
-        </Slide>
+
     </Carousel>
 </template>
 
@@ -18,10 +24,47 @@ export default {
     components: {
         Carousel,
         Slide
+    },
+    props: {
+        infos: Array
+    },
+    data() {
+        return {
+            navigation: true
+        }
+    },
+    methods: {
+        OnResize() {
+            if (window.innerWidth < 768) {
+                this.navigation = false;
+            } else {
+                this.navigation = true;
+            }
+        }
+    },
+    mounted() {
+        this.OnResize();
+        window.addEventListener('resize', this.OnResize, {passive: true});
     }
 }
 </script>
 
-<style lang='scss' scoped>
+<style lang='scss'>
+
+@import '../../assets/style/variables.scss';
+
+.VueCarousel {
+    width: 80%;
+
+    .VueCarousel-dot {
+        border: 1px solid $mainColor;
+        padding: 0 !important;
+        margin: 0 5px;
+
+        &--active{
+            background-color: $mainColor !important;
+        }
+    }
+}
 
 </style>
--- src/components/sections/Designe.vue
+++ src/components/sections/Designe.vue
@@ -7,7 +7,7 @@
             <p class="text-center">Build more and spend less time managing your infrastructure with our easy-to-use control panel and API.</p>
 
             <!-- carousel -->
-            <CustomSlider/>
+            <CustomSlider :infos="slides" class="mx-auto"/>
 
         </div>
     </section>
@@ -20,6 +20,28 @@ export default {
     name: 'Designe',
     components: {
         CustomSlider
+    },
+    data() {
+        return {
+            slides: [
+                {
+                    image: require('../../assets/img/slide.jpg'),
+                    desc: 'Create compute servers in 55 seconds or less.'
+                },
+                {
+                    image: require('../../assets/img/slide.jpg'),
+                    desc: 'Create compute servers in 55 seconds or less.'
+                },
+                {
+                    image: require('../../assets/img/slide.jpg'),
+                    desc: 'Create compute servers in 55 seconds or less.'
+                },
+                {
+                    image: require('../../assets/img/slide.jpg'),
+                    desc: 'Create compute servers in 55 seconds or less.'
+                }
+            ]
+        }
     }
 }
 </script>
@@ -30,6 +52,10 @@ export default {
 
 section {
     background-color: $sectionColor;
+
+    p {
+        color: $textColor;
+    }
 }
 
 </style>
