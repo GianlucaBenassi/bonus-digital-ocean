c92026220d389cf23e9b34ec05a9d3b855fdd115
Gianluca Benassi <benox1199@gmail.com>
2022-01-07 10:40:41 +0100

    GetStart form

--- src/components/sections/GetStartTop.vue
+++ src/components/sections/GetStartTop.vue
@@ -10,8 +10,23 @@
                 </div>
 
                 <!-- google login form -->
-                <div class="col-12">
+                <div class="col-12 pb-5">
+                    <form class="rounded pt-4 pb-2 px-2" @submit.prevent>
 
+                        <h4 class="fw-bold text-center">Deploy your app in seconds.</h4>
+                        <h4 class="fw-bold text-center ms_color">Get started now.</h4>
+
+                        <input class="w-100 mb-2 px-1" type="email" placeholder="Email">
+                        <input class="w-100 mb-2 px-1" type="password" placeholder="Password">
+
+                        <button class="d-block rounded w-100 mb-2 ms_create">Create your account</button>
+                        <button class="d-block position-relative border rounded w-100 mb-2 ms_google">
+                            <img class="position-absolute top-50 translate-middle" src="../../assets/img/google-icon.png" alt="google logo">
+                            Sing up with google
+                        </button>
+
+                        <p class="text-center">By signign up you agree to the <a href="#">Terms of Service</a>.</p>
+                    </form>
                 </div>
 
             </div>
@@ -27,15 +42,58 @@ export default {
 
 <style lang='scss' scoped>
 
+@import '../../assets/style/variables.scss';
+
 section {
     background-image: url('../../assets/img/bg-banner-home.jpg');
     background-position: center;
     background-size: cover;
 
-    height: 600px;
-
     .ms_description {
         color: #fff;
+    }
+
+    form {
+        background-color: #fff;
+
+        .ms_color {
+            color: $mainColor;
+        }
+
+        .ms_create {
+            color: #fff;
+            background-color: #579aff;
+            border: 1px solid #579aff;
+            transition: all .3s;
+
+            &:hover {
+                color: $mainColor;
+                background-color: #fff;
+            }
+        }
+
+        .ms_google {
+            background-color: #fff;
+            transition: all .3s;
+            
+            img {
+                max-height: 16px;
+                left: 14px;
+
+            }
+
+            &:hover {
+                background-color: #efefef;
+            }
+        }
+
+        p {
+            font-size: 12px;
+
+            a:hover {
+                text-decoration: underline;
+            }
+        }
     }
 }
 
