38da98643216f5ff8402d4f3addc0c35d295c1a1
Gianluca Benassi <benox1199@gmail.com>
2022-01-07 20:52:54 +0100

    Features section structure

---
+++ src/components/sections/Features.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+        
+        <div class="container py-5">
+            
+            <h2 class="text-center fw-bold">The features your business needs</h2>
+
+            <div class="row row-cols">
+                
+                <div class="col">
+
+                </div>
+                
+                <div class="col">
+
+                </div>
+                
+                <div class="col">
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Features'
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
