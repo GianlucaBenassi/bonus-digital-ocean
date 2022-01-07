ce4fbf1d82ba5376d5bab58981e3ba7ca6b8243f
Gianluca Benassi <benox1199@gmail.com>
2022-01-07 11:31:47 +0100

    Deploy section list

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -2,18 +2,21 @@
     <main>
         <GetStartTop/>
         <Businesses/>
+        <Deploy/>
     </main>
 </template>
 
 <script>
 import GetStartTop from '../sections/GetStartTop.vue';
 import Businesses from '../sections/Businesses.vue';
+import Deploy from '../sections/Deploy.vue';
 
 export default {
     name: 'Main',
     components: {
         GetStartTop,
-        Businesses
+        Businesses,
+        Deploy
     }
 }
 </script>
---
+++ src/components/sections/Deploy.vue
@@ -0,0 +1,45 @@
+<template>
+    <section>
+        <div class="container py-5">
+
+            <!-- title -->
+            <h2 class="text-center fw-bold">Deploy and scale seamlessly</h2>
+            <p class="text-center">Our optimized configuration process saves your team time when running ans scaling distributed applications, AI & machine learning workloads, hosted services, client websites, or CI/CD environments.</p>
+
+            <div class="row">
+
+                <!-- list column -->
+                <div class="col-12">
+                    <ul class="ps-0">
+                        <li class="border-start ps-2 py-1">Deploy</li>
+                        <li class="border-start ps-2 py-1">Manage</li>
+                        <li class="border-start ps-2 py-1">Secure</li>
+                        <li class="border-start ps-2 py-1">Scale</li>
+                        <li class="border-start ps-2 py-1">Monitor</li>
+                    </ul>
+                </div>
+
+                <!-- description column-->
+                <div class="col-12">
+                    descrizione
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Deploy'
+}
+</script>
+
+<style lang='scss' scoped>
+
+p, li {
+    color: #606e8c;
+}
+
+</style>
