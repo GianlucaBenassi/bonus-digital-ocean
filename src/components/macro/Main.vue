6e505bfd6e2d67c7039ca90f50e2f54ae0eb4a33
Gianluca Benassi <benox1199@gmail.com>
2022-01-07 11:13:45 +0100

    Businesses section

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,16 +1,19 @@
 <template>
     <main>
         <GetStartTop/>
+        <Businesses/>
     </main>
 </template>
 
 <script>
 import GetStartTop from '../sections/GetStartTop.vue';
+import Businesses from '../sections/Businesses.vue';
 
 export default {
     name: 'Main',
     components: {
-        GetStartTop
+        GetStartTop,
+        Businesses
     }
 }
 </script>
