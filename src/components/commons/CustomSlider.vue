992bcb5059bf841e3f12e120107f61000f18f941
Gianluca Benassi <benox1199@gmail.com>
2022-01-07 14:49:29 +0100

    Vue carousel installation

---
+++ src/components/commons/CustomSlider.vue
@@ -0,0 +1,27 @@
+<template>
+    <Carousel>
+        <Slide>
+            contenuto
+        </Slide>
+        <Slide>
+            contenuto
+        </Slide>
+    </Carousel>
+</template>
+
+<script>
+
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+    name: 'CustomSlider',
+    components: {
+        Carousel,
+        Slide
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
--- src/components/sections/Designe.vue
+++ src/components/sections/Designe.vue
@@ -6,14 +6,21 @@
             <h2 class="fw-bold text-center">Designed for developers</h2>
             <p class="text-center">Build more and spend less time managing your infrastructure with our easy-to-use control panel and API.</p>
 
+            <!-- carousel -->
+            <CustomSlider/>
 
         </div>
     </section>
 </template>
 
 <script>
+import CustomSlider from '../commons/CustomSlider.vue';
+
 export default {
-    name: 'Designe'
+    name: 'Designe',
+    components: {
+        CustomSlider
+    }
 }
 </script>
 
